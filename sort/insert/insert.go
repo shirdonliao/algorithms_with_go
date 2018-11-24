eb48e0dc086332cff32587ac9b1f48e7bf27066f
shirdonliao <toingart@163.com>
2018-11-24 13:36:26 +0800

    插入排序法提交

---
+++ sort/insert/insert.go
@@ -0,0 +1,31 @@
+package insert
+
+import (
+	"../../common"
+	"fmt"
+)
+
+func main() {
+	arr := common.GetArr(5, 20)
+	arr = []int{29, 10, 14, 37, 14}
+	fmt.Println("[UNSORTED_ARR]: ", arr)
+
+	n := len(arr)
+	if n <= 1 {
+		fmt.Println("[ALREADY SORTED]: ", arr)
+		return
+	}
+
+	// 遍历所有元素
+	for i := 1; i < n; i++ {
+		// 向前找位置
+		for j := i; j > 0; j-- {
+			// 合适位置插入
+			if arr[j-1] > arr[j] {
+				arr[j-1], arr[j] = arr[j], arr[j-1]
+			}
+		}
+		fmt.Println("[DEBUGING]: ", arr)
+	}
+	fmt.Println("[SORTED_ARR]: ", arr)
+}
